90a2e05b476d0b548e99fda4ef3c98064c3f4a66
Juan Jesus Mata Solis <jujemataso@ittepic.edu.mx>
2023-03-28 11:34:25 -0700

    CRUD GUI COMPLETED

---
+++ src/app/services/student.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+
+import { Student } from '../models/student';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StudentService {
+  private students: Student[] = [];
+  constructor() {
+    this.students.push(
+      {
+        controlNumber: 'C17400607',
+        age: 23,
+        career: 'ISC',
+        curp: 'MASJ990901HNTTLN04',
+        email: 'jujemataso@ittepic.edu.mx',
+        name: 'Juan Jesús Mata Solis',
+        nip: 4849,
+        photo: 'https://picsum.photos/600?random=1',
+      },
+      {
+        controlNumber: '18400607',
+        age: 23,
+        career: 'ISC',
+        curp: 'MASJ990901HNTTLN10',
+        email: 'jejusomata@ittepic.edu.mx',
+        name: 'Jesus Juan Solis Mata',
+        nip: 4849,
+        photo: 'https://picsum.photos/600?random=2',
+      }
+    );
+  }
+
+  public getStudents(): Student[] {
+    return this.students;
+  }
+
+  public getStudentByControlNumber(controlNumber: string): Student | undefined {
+    return this.students.find(
+      (student) => student.controlNumber === controlNumber
+    );
+  }
+
+  public newStudent(student: Student): Student[] {
+    this.students.push(student);
+    return this.students;
+  }
+
+  public updateStudent(pos: number, student: Student): Student[] {
+    this.students[pos] = student;
+    return this.students;
+  }
+
+  public deleteStudent(pos: number): Student[] {
+    this.students.splice(pos, 1);
+    return this.students;
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
 
+import { Carrera } from '../models/carrera';
+import { Student } from '../models/student';
+import { CarrerasService } from '../services/carreras.service';
+import { StudentService } from '../services/student.service';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  students: Student[] = [];
+  student: Student = {
+    controlNumber: '',
+    age: 0,
+    career: 'ISC',
+    curp: '',
+    email: '',
+    name: '',
+    nip: 0,
+    photo: '',
+  };
+  carreras: Carrera[] = [];
+  constructor(
+    private studentService: StudentService,
+    private carreraService: CarrerasService,
+  ) {
+    this.students = this.studentService.getStudents();
+    this.carreras = this.carreraService.getCarreras();
+  }
 
-  constructor() {}
+  public deleteStudent(pos: number) {
+    console.log(pos)
+    this.students = this.studentService.deleteStudent(pos);
+  }
 
+  public newStudent() {
+    this.student.photo = `https://picsum.photos/600?random=${
+      this.students.length + 1
+    }`;
+    this.students = this.studentService.newStudent({ ...this.student });
+  }
 }
