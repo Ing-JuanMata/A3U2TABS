0f9405f1f5a86ff2db8e1ef26c4812a45888e4e2
Juan Jesus Mata Solis <jujemataso@ittepic.edu.mx>
2023-03-28 11:56:46 -0700

    SOME DEV

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 import { Carrera } from '../models/carrera';
 import { Student } from '../models/student';
@@ -26,13 +27,15 @@ export class Tab1Page {
   constructor(
     private studentService: StudentService,
     private carreraService: CarrerasService,
+    private alertController: AlertController,
+    private toastController: ToastController
   ) {
     this.students = this.studentService.getStudents();
     this.carreras = this.carreraService.getCarreras();
   }
 
   public deleteStudent(pos: number) {
-    console.log(pos)
+    this.confirmationDialog('¿Estás seguro de eliminar este estudiante?', () => {});
     this.students = this.studentService.deleteStudent(pos);
   }
 
@@ -41,5 +44,46 @@ export class Tab1Page {
       this.students.length + 1
     }`;
     this.students = this.studentService.newStudent({ ...this.student });
+  }
+
+  private async presentToast(
+    message: string,
+    color: 'success' | 'danger' | 'warning'
+  ) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 500,
+      color,
+    });
+    toast.present();
+  }
+
+  private async confirmationDialog(
+    header: string,
+    handler?: Function,
+    dismissFunction?: Function
+  ) {
+    const alert = await this.alertController.create({
+      header,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+        },
+        {
+          text: 'Confirmar',
+          role: 'confirm',
+          cssClass: 'primary',
+          handler: () => {
+            if (handler) handler();
+          },
+        },
+      ],
+    });
+    alert.present();
+    alert.onDidDismiss().then((respuesta) => {
+      if (dismissFunction) dismissFunction(respuesta);
+    });
   }
 }
